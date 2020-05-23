84c952af00553b4c6403971b4b3365b6bd9455f2
Anna Guerrero <acg2176@columbia.edu>
2020-05-23 19:59:13 +0000

    trial code for api

--- symptomata/lib/symptomata/api.rb
+++ symptomata/lib/symptomata/api.rb
@@ -1,4 +1,8 @@
-class Symptomata::API
-  #def get_api
-    
-end
+require 'oauth2'
+
+site_path = 'https://sandbox-authservice.priaid.ch/'
+redirect_uri = "http://mygemserver.com"
+client = OAuth2::Client.new(ENV[''])
+
+
+
