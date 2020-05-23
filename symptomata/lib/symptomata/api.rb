d2c5e4d057732baaa3442eaf47e81de950bd0eb0
Anna Guerrero <acg2176@columbia.edu>
2020-05-23 20:41:07 +0000

    more modifications

--- symptomata/lib/symptomata/api.rb
+++ symptomata/lib/symptomata/api.rb
@@ -3,7 +3,8 @@ require 'pry'
 
 site_path = 'https://sandbox-authservice.priaid.ch/'
 redirect_uri = "http://mygemserver.com"
-client = OAuth2::Client.new(ENV[''])
+client = OAuth2::Client.new(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], :site => site_path)
+client.auth_code.authorize_url(:redirect_uri => redirect_uri)
 
 # require 'httparty'
 
