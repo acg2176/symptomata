2b6970c11f4bb746c42452a841f68593db773b40
Anna Guerrero <acg2176@columbia.edu>
2020-05-23 20:39:23 +0000

    modify api.rb'

--- symptomata/lib/symptomata/api.rb
+++ symptomata/lib/symptomata/api.rb
@@ -1,8 +1,13 @@
 require 'oauth2'
+require 'pry'
 
 site_path = 'https://sandbox-authservice.priaid.ch/'
 redirect_uri = "http://mygemserver.com"
 client = OAuth2::Client.new(ENV[''])
 
+# require 'httparty'
 
 
+# site = HTTParty.get("https://covidtracking.com/api/v1/states/current.json")
+
+binding.pry
